3f301329c4314b5864b5747f7238f14d8d4615e9
jpb <jpb.06@outlook.com>
2019-07-01 20:49:26 +0200

    Reading queue...

--- src/events/discord.client.ready.event.ts
+++ src/events/discord.client.ready.event.ts
@@ -1,6 +1,9 @@
-﻿import { Client } from 'discord.js';
+﻿import { Client, TextChannel } from 'discord.js';
 
 import { Logger } from '../util/logging.helper';
+import { Queuing } from 'node-message-broker';
+import { Types } from 'dowpro-replay-watcher-dal';
+import { EmbedHelper } from '../util/embed.helper';
 //import { generateInviteLink } from './../../businesslogic/util/invite.link.helper';
 
 export abstract class ClientReadyEvent {
@@ -10,6 +13,22 @@ export abstract class ClientReadyEvent {
     ): Promise<void> {
         try {
             await client.user.setActivity('DoWpro', { type: 'PLAYING' });
+
+            await Queuing.popFrom<Types.QueuedReplay>('incoming-games', async (game: Types.QueuedReplay) => {
+                console.log(game);
+
+                let embed = await EmbedHelper.notifyGameResult(game);
+
+                let channel = client.guilds.first().channels.find(el => el.name === 'crevette-bot-tests');
+                if (channel && channel.type === "text") {
+                    (<TextChannel>channel).send({ embed: embed });
+                }
+            });
+
+            //let channel = client.guilds.first().channels.find(el => el.name === 'crevette-bot-tests');
+            //if (channel && channel.type === "text") {
+            //    (<TextChannel>channel).send('uwu');
+            //}
 
             // let link = generateInviteLink(client);
 
